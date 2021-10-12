e8d785db6bd7542d9fe7678c8d0a4e1cf25f02bd
hkdhakarey <hkdhakarey@gmail.com>
2021-10-13 01:29:11 +0530

    Second

---
+++ src/app/ImageGallary/image-gallary/image-gallary.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ImageGallaryComponent } from './image-gallary.component';
+
+describe('ImageGallaryComponent', () => {
+  let component: ImageGallaryComponent;
+  let fixture: ComponentFixture<ImageGallaryComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ImageGallaryComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ImageGallaryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextEditorComponent } from './TextEditor/text-editor/text-editor.component';
+import { SendMailComponent } from './Mail/send-mail/send-mail.component';
+import { ImageGallaryComponent } from './ImageGallary/image-gallary/image-gallary.component';
+import { ExportToExcelComponent } from './Excel/export-to-excel/export-to-excel.component';
+import { ImportExcelComponent } from './Excel/import-excel/import-excel.component';
+import { EmployeeMasterComponent } from './Employee/employee-master/employee-master.component';
+import { EmployeeListComponent } from './Employee/employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextEditorComponent,
+    SendMailComponent,
+    ImageGallaryComponent,
+    ExportToExcelComponent,
+    ImportExcelComponent,
+    EmployeeMasterComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
